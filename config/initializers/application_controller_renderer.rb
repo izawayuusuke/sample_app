5eb5bf1d1c14c1cf1a8e3239aea90a4b6dab9bdd
izawayuusuke <yuu0502@gmail.com>
2020-02-19 09:56:15 +0000

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1d280a13488e67adf199be13269b66e6f7c06816272fca98f7bb6585887b98c4d363656345795396881d0cf928b19b21a53d983400cff62dd9dbfa341ce2b9d5'
